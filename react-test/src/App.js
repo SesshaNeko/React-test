6773eef2b7f5ddd8dca27fdf07b7a14342905314
SesshaNeko <Stepan1802@mail.ru>
2021-10-10 18:26:47 +0400

    modification

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,27 +1,55 @@
-import React from "react";
-import TodoList from "./Todo/TodoList";
+import React from "react"
+import TodoList from "./Todo/TodoList"
+import Context from "./context"
+import AddTodo from "./Todo/AddTodo"
 
 function App() {
-  var todos = [
+  const [todos, setTodos] =  React.useState([
     {id:1, completed: false, title: 'Купить хлеб'},
     {id:2, completed: false, title: 'Купить молоко'},
     {id:3, completed: false, title: 'Купить броколи'},
-  ]
+    ])
+  
 
   function toggleTodo(id){
-    todos = todos.map(todo => {
-      if(todo.id === id){
+    setTodos(
+      todos.map(todo => {
+        if(todo.id === id){
         todo.completed= !todo.completed
-      }
-      return todo
-    })
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id){
+    setTodos(todos.filter(todo=>todo.id !==id))
+  }
+
+  function addTodo(title){
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    )
   }
 
   return (
-    <div className='wrapper'>
-      <h1>React Test</h1>
-      <TodoList todos={todos} onToggle={toggleTodo}></TodoList>
-    </div>  
+    <Context.Provider value={{removeTodo}}>
+      <div className='wrapper'>
+        <h1>React Test</h1>
+
+        <AddTodo onCreate={addTodo}/>
+
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}></TodoList> : <p>NO TODOS!</p>}
+
+        
+      </div>  
+    </Context.Provider>
   )
 }
 
