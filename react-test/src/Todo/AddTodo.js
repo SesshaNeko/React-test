0538cca0365f4c251eb795f972d2cddaeb8d622e
SesshaNeko <Stepan1802@mail.ru>
2021-10-11 17:22:42 +0400

    modification

--- react-test/src/Todo/AddTodo.js
+++ react-test/src/Todo/AddTodo.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react"
 import PropTypes from 'prop-types'
 
-function AddTodo({onCreate}){
+function AddTodo({onCreate} ){
     const [value, setValue]=useState('')
 
     function submitHandler(event){
